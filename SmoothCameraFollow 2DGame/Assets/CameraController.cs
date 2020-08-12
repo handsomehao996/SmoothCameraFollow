bd522102657f38ec2120fb9dd35c903db39be18d
GameHao <1812677782@qq.com>
2020-08-12 10:09:31 +0800

    Add a very cool 2D Game

---
+++ SmoothCameraFollow 2DGame/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothSpeed = 3f;
+
+    private void LateUpdate()
+    {
+        Vector3 newVec = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newVec, smoothSpeed * Time.deltaTime);
+        transform.LookAt(target);
+    }
+}
---
+++ SmoothCameraFollow 2DGame/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator ani;
+    public SpriteRenderer spriteRenderer;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal") * moveSpeed;
+        rb.velocity = new Vector2(horMove, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.x > 0.1)
+        {
+            spriteRenderer.flipX = false;
+        }
+        if(rb.velocity.x < -0.1)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        ani.SetFloat("VelocityXabs", Mathf.Abs(rb.velocity.x));
+        ani.SetFloat("VelocityYabs", Mathf.Abs(rb.velocity.y));
+        ani.SetFloat("VelocityY", rb.velocity.y);
+    }
+}
