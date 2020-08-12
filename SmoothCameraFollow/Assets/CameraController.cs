2e4ce811ce988bb172f754b3ed5dae4bd54e7833
GameHao <1812677782@qq.com>
2020-08-12 09:21:51 +0800

    Create a very cool camera

---
+++ SmoothCameraFollow/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        Vector3 newVec = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newVec, smoothSpeed * Time.deltaTime);
+        transform.LookAt(target);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    Vector3 newVec = target.position + offset;
+    //    transform.position = Vector3.Lerp(transform.position, newVec, smoothSpeed * Time.fixedDeltaTime);
+    //    transform.LookAt(target);
+    //}
+}
---
+++ SmoothCameraFollow/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 10f;
+
+    private void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        Vector3 moveVec = Vector3.right * hor;
+        rb.MovePosition(rb.position + moveVec * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
