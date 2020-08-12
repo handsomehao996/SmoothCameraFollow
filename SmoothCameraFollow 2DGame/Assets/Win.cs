54356e2bf569cb84d9bcf077869090874692ce62
GameHao <1812677782@qq.com>
2020-08-12 14:46:41 +0800

    Try to make it fun

---
+++ SmoothCameraFollow 2DGame/Assets/Win.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public GameObject winPanel;
+    public GameObject winEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            winPanel.SetActive(true);
+            winEffect.SetActive(true);
+        }
+    }
+}
